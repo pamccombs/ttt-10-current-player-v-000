b4a9fe78f696316935cc15ef61ccc032e1146df8
Patrick McCombs <pa.mccombs1@gmail.com>
2018-04-02 16:40:58 +0000

    Done.

---
+++ lib/current_player.rb
@@ -0,0 +1,35 @@
+#turn_count
+def turn_count(board)
+  counter = 0
+  board.each { |turns| if turns == "X" 
+  then counter += 1 
+  elsif turns == "O" 
+  then counter += 1
+else
+  counter += 0
+end
+}
+counter
+end
+
+
+#current_player
+def current_player(board)
+  if turn_count(board) % 2 == 0
+    "X"
+  else
+    "O"
+  end
+ # "#{turn_count(board)}"
+end
+
+#helper_method
+def position_taken?(board, index)
+  if board[index] == " " || board[index] == "" || board[index] == nil
+    then false
+     elsif board[index] == "X" || "O"
+    then true
+  else
+    true
+  end
+end
