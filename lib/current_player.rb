345be71c3c965f78a84ae60627d52a9de3523204
Patrick McCombs <pa.mccombs1@gmail.com>
2018-04-02 16:47:08 +0000

    Done.

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -14,13 +14,13 @@ end
 
 
 #current_player
-def current_player(board)
-  if turn_count(board) % 2 == 0
-    "X"
-  else
-    "O"
-  end
- # "#{turn_count(board)}"
+def current_player(board) turn_count(board) % 2 == 0? "X" : "O"
+  
+  #if turn_count(board) % 2 == 0
+  #  "X"
+  #else
+  #  "O"
+  #end
 end
 
 #helper_method
