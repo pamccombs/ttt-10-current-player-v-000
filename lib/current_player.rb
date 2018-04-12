0d8559e33980104fbd37e441b71a900a66cee685
Patrick McCombs <pa.mccombs1@gmail.com>
2018-04-12 16:02:12 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -14,13 +14,13 @@ end
 
 
 #current_player
-def current_player(board) turn_count(board) % 2 == 0? "X" : "O"
+def current_player(board) turn_count(board).even "X" : "O"
   
-  #if turn_count(board) % 2 == 0
-  #  "X"
-  #else
-  #  "O"
-  #end
+  if turn_count(board).even? #% 2 == 0
+    "X"
+  else
+    "O"
+  end
 end
 
 #helper_method
